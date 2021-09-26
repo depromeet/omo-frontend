2d9efeca9167777ed8b045b6817db5d6c5370bcf
KG <acrkdduf@naver.com>
2021-09-26 18:52:10 +0900

    매장 미리보기 컴포넌트 스타일 수정 및 적용

--- src/components/StoreDisplay/StoreDisplay.tsx
+++ src/components/StoreDisplay/StoreDisplay.tsx
@@ -1,32 +1,37 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import * as S from './styles';
 
 export interface StoreDisplayProps {
-  id?: number;
+  id: number;
+  types: string[];
   image: string;
-  location: string;
-  type: string;
   name: string;
   desc: string;
 }
 
-const StoreDisplay = ({ image, location, type, name, desc }: StoreDisplayProps) => {
+const StoreDisplay = ({ id, image, types, name, desc }: StoreDisplayProps) => {
   return (
     <S.StoreDisplay>
-      <S.StoreImageWrapper>
-        <S.SubTitles>
-          <span>{location}</span>
-          <span>{type}</span>
-        </S.SubTitles>
+      <Link href={`/search/${id}`} passHref>
+        <a>
+          <S.StoreImageWrapper>
+            <S.SubTitles>
+              {types.map((type) => (
+                <span key={type}>{type}</span>
+              ))}
+            </S.SubTitles>
 
-        <Image src={image} alt="매장 이미지 미리보기" layout="fill" />
-      </S.StoreImageWrapper>
+            <Image src={image} alt="매장 이미지 미리보기" layout="fill" />
+          </S.StoreImageWrapper>
 
-      <S.StoreDescriptionWrapper>
-        <h1 className="store-title">{name}</h1>
-        <pre className="store-desc">{desc}</pre>
-      </S.StoreDescriptionWrapper>
+          <S.StoreDescriptionWrapper>
+            <h1 className="store-title">{name}</h1>
+            <pre className="store-desc">{desc}</pre>
+          </S.StoreDescriptionWrapper>
+        </a>
+      </Link>
     </S.StoreDisplay>
   );
 };
