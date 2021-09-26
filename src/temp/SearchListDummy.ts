94404d14716c9911ebd8a8836925cd09e4447e9c
KG <acrkdduf@naver.com>
2021-09-26 18:48:52 +0900

    search 페이지에서 사용할 더미데이터 임시 파일 작성

---
+++ src/temp/SearchListDummy.ts
@@ -0,0 +1,37 @@
+import { DetailPageProps } from '@pages/search/[id]';
+
+export const dummys: DetailPageProps[] = [
+  {
+    id: 1,
+    image: `https://c2.poing.co.kr/place/image/thumbnail/MjAyMTA5/163100066261371856c2830.jpg`,
+    types: ['강남구', '하이엔드'],
+    name: `스시조`,
+    desc: `오마카세 경력 25년의 셰프와 산지직송
+제철 식자재의 정성이 담긴 요리`,
+    location: '서울특별시 강남구 어쩌고 저쩌구동 저쩌구 상가 A동 2342124호',
+    tel: '02) 12345-67890',
+    price: '디너 300,000 ~',
+  },
+  {
+    id: 2,
+    image: `https://c2.poing.co.kr/place/image/service/MjAyMTA5/163101073861373fb28a0a2.jpg`,
+    types: ['강남구', '하이엔드'],
+    name: `스시조어진`,
+    desc: `오마카세 경력 25년의 셰프와 산지직송
+제철 식자재의 정성이 담긴 요리`,
+    location: '서울특별시 강남구 어쩌고 저쩌구동 저쩌구 상가 B동 24호',
+    tel: '02) 010-67890',
+    price: '디너 250,000 ~',
+  },
+  {
+    id: 3,
+    image: `https://c2.poing.co.kr/place/image/service/MjAyMTA5/1631000742613718a614ac5.jpg`,
+    types: ['은평구', '하이엔드'],
+    name: `조어진스시`,
+    desc: `오마카세 경력 25년의 셰프와 산지직송
+제철 식자재의 정성이 담긴 요리`,
+    location: '서울특별시 은평구 저기 어딘가',
+    tel: '02) 15-690',
+    price: '디너 1,300,000 ~',
+  },
+];
