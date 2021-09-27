db071c563e2d39e10d5373df863cd73b8f0b12f5
HyojinKim <gywls00100@gmail.com>
2021-09-27 19:15:36 +0900

    닉네임변경 페이지 작성

---
+++ src/pages/mypage/changeNicknName.tsx
@@ -0,0 +1,10 @@
+// 닉네임 변경 페이지
+import styled from 'styled-components';
+
+import Layout from '@components/Layout';
+
+const changeNickName = () => {
+  return <Layout title="닉네임변경">닉네임변경</Layout>;
+};
+
+export default changeNickName;
--- src/pages/mypage/changeNickname.tsx
+++
@@ -1,7 +0,0 @@
-// 닉네임 변경 페이지
-
-const changeNickname = () => {
-  return <div></div>;
-};
-
-export default changeNickname;
