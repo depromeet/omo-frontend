c419ec707839c7c0f7640a1c976dbd69ceb2cee8
HyojinKim <gywls00100@gmail.com>
2021-09-28 01:25:50 +0900

    프로필사진 변경 alert 추가

--- src/pages/mypage/changePhoto.tsx
+++ src/pages/mypage/changePhoto.tsx
@@ -4,8 +4,10 @@ import styled from 'styled-components';
 import ModalLayout from '@components/Layout/ModalLayout';
 
 const changePhoto = () => {
+  const onClick = () => alert('프로필사진 변경이 완료되었습니다.');
+
   return (
-    <ModalLayout title="프로필사진 변경" buttonContent="확인">
+    <ModalLayout title="프로필사진 변경" buttonContent="확인" clickHandler={onClick}>
       프로필사진 변경
     </ModalLayout>
   );
