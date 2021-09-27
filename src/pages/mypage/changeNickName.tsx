959c7b23beaeba24755c25744dcb5063a84b845c
HyojinKim <gywls00100@gmail.com>
2021-09-28 01:26:02 +0900

    닉네임 변경 alert 추가

--- src/pages/mypage/changeNickName.tsx
+++ src/pages/mypage/changeNickName.tsx
@@ -1,10 +1,54 @@
 // 닉네임 변경 페이지
+import { useState } from 'react';
 import styled from 'styled-components';
 
 import ModalLayout from '@components/Layout/ModalLayout';
 
-const changeNickName = () => {
-  return <ModalLayout title="닉네임변경" buttonContent="확인">닉네임변경</ModalLayout>;
+const ChangeNickName = () => {
+  const [text, setText] = useState('');
+
+  const onClick = () => alert(`변경된 닉네임 : ${text}`);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert(`변경된 닉네임 : ${text}`);
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setText(value);
+  };
+  return (
+    <ModalLayout title="닉네임변경" buttonContent="확인" clickHandler={onClick}>
+      <ChangeNickNamePage>
+        <Title>변경할 닉네임을 입력해주세요!</Title>
+        <form onSubmit={onSubmit}>
+          <Input type="text" value={text} onChange={onChange} />
+        </form>
+      </ChangeNickNamePage>
+    </ModalLayout>
+  );
 };
 
-export default changeNickName;
+export default ChangeNickName;
+
+const ChangeNickNamePage = styled.section`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  padding: 1rem;
+`;
+
+const Title = styled.h1`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const Input = styled.input`
+  height: 40px;
+  width: 90%;
+  font-size: 18px;
+  outline: none;
+  border: none;
+  border-bottom: 1.4px solid rgb(152, 152, 152);
+`;
