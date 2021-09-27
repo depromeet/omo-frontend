b65a3e3d3663eba22daa794bfe0abf8d410c5b6a
HyojinKim <gywls00100@gmail.com>
2021-09-27 19:29:52 +0900

    프로필사진 변경 페이지 작성

--- src/pages/mypage/changePhoto.tsx
+++ src/pages/mypage/changePhoto.tsx
@@ -1,7 +1,10 @@
 // 사진 수정 페이지
+import styled from 'styled-components';
+
+import Layout from '@components/Layout';
 
 const changePhoto = () => {
-  return <div></div>;
+  return <Layout title="프로필사진 변경">프로필사진 변경</Layout>;
 };
 
 export default changePhoto;
