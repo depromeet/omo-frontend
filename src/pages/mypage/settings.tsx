8314c8fcbb9577df4ec56f8035edf308aea334f4
HyojinKim <gywls00100@gmail.com>
2021-09-27 19:30:05 +0900

    프로필사진 변경, 닉네임 변경 페이지 링크 걸기

--- src/pages/mypage/settings.tsx
+++ src/pages/mypage/settings.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styled from 'styled-components';
 
 // 개인정보 설정 페이지
@@ -8,8 +9,16 @@ const settings = () => {
     <Layout title="개인정보 설정">
       <SettingSection>로그아웃</SettingSection>
       <SettingSection>회원탈퇴</SettingSection>
-      <SettingSection>닉네임 변경</SettingSection>
-      <SettingSection>프로필사진 변경</SettingSection>
+      <SettingSection>
+        <Link href="/mypage/changeNickName" passHref>
+        <a className="setting-link">닉네임 변경</a>
+        </Link>
+      </SettingSection>
+      <SettingSection>
+        <Link href="/mypage/changePhoto" passHref>
+          <a className="setting-link">프로필사진 변경</a>
+        </Link>
+      </SettingSection>
     </Layout>
   );
 };
@@ -23,4 +32,10 @@ const SettingSection = styled.div`
   height: 55px;
   padding: 0 21px;
   border-bottom: 1px solid rgba(196, 196, 196, 0.2);
+  text-decoration: none;
+
+  .setting-link {
+    text-decoration: none;
+    color: #000;
+  }
 `;
