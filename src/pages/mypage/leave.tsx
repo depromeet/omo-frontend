4bd5da137dd8bd08ed6723868039c10cb666e980
HyojinKim <gywls00100@gmail.com>
2021-09-27 20:20:36 +0900

    회원탈퇴 페이지 작성

---
+++ src/pages/mypage/leave.tsx
@@ -0,0 +1,10 @@
+// 회원탈퇴 페이지
+import styled from 'styled-components';
+
+import ModalLayout from '@components/Layout/ModalLayout';
+
+const leave = () => {
+  return <ModalLayout title="회원탈퇴">회원탈퇴</ModalLayout>;
+};
+
+export default leave;
