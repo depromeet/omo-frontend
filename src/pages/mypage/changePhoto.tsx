041b2aa1b19a0d087ac356451f0ff5f6f23bdebb
HyojinKim <gywls00100@gmail.com>
2021-09-28 01:40:09 +0900

    프로필 사진들 임시영역 추가

--- src/pages/mypage/changePhoto.tsx
+++ src/pages/mypage/changePhoto.tsx
@@ -8,9 +8,52 @@ const changePhoto = () => {
 
   return (
     <ModalLayout title="프로필사진 변경" buttonContent="확인" clickHandler={onClick}>
-      프로필사진 변경
+      <ChangePhotoPage>
+        <PhotoContainer>
+          <div className="photo-wrapper"></div>
+          <div className="photo-wrapper"></div>
+          <div className="photo-wrapper"></div>
+          <div className="photo-wrapper"></div>
+          <div className="photo-wrapper"></div>
+          <div className="photo-wrapper"></div>
+          <div className="photo-wrapper"></div>
+          <div className="photo-wrapper"></div>
+        </PhotoContainer>
+        <ChangeButton>내 사진으로 할래요</ChangeButton>
+      </ChangePhotoPage>
     </ModalLayout>
   );
 };
 
 export default changePhoto;
+
+const ChangePhotoPage = styled.section`
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`;
+
+const PhotoContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  gap: 15px;
+  margin-bottom: 1rem;
+
+  .photo-wrapper {
+    width: 70px;
+    height: 70px;
+    border-radius: 50%;
+    background-color: #c4c4c4;
+  }
+`;
+const ChangeButton = styled.button`
+  padding: 15px;
+  background-color: #e1e1e1;
+  outline: none;
+  border: none;
+  border-radius: 8px;
+  color: #4b4b4b;
+  font-size: 16px;
+`;
