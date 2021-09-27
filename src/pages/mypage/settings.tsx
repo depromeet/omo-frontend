db371c000747d7a9a768e020c374d0658501bf36
HyojinKim <gywls00100@gmail.com>
2021-09-27 19:05:14 +0900

    setting페이지에서 페이지 이동할 수 있는 section 나누기

--- src/pages/mypage/settings.tsx
+++ src/pages/mypage/settings.tsx
@@ -1,7 +1,26 @@
+import styled from 'styled-components';
+
 // 개인정보 설정 페이지
 import Layout from '@components/Layout';
+
 const settings = () => {
-  return <Layout title="개인정보 설정">로그아웃</Layout>;
+  return (
+    <Layout title="개인정보 설정">
+      <SettingSection>로그아웃</SettingSection>
+      <SettingSection>회원탈퇴</SettingSection>
+      <SettingSection>닉네임 변경</SettingSection>
+      <SettingSection>프로필사진 변경</SettingSection>
+    </Layout>
+  );
 };
 
 export default settings;
+
+const SettingSection = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 18px;
+  height: 55px;
+  padding: 0 21px;
+  border-bottom: 1px solid rgba(196, 196, 196, 0.2);
+`;
