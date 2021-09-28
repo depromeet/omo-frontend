3ca18421b1e8d4f6281bd8b3b84b9b23b48b3c9a
HyojinKim <gywls00100@gmail.com>
2021-09-28 15:52:51 +0900

    원상복구

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,30 +1,29 @@
 // 마이페이지 메인
 import Link from 'next/link';
+import styled from 'styled-components';
 
 import MyPageLayout from '@components/Layout/MyPageLayout';
 import VisitedStore from '@components/VisitedStore';
 import { dummys } from '@temp/VisitedStoreDummy';
 
-import * as S from './styles';
-
 const index = () => {
   return (
     <MyPageLayout title="마이페이지">
-      <S.Header>
+      <Header>
         <div className="profile">
           <div className="profile-image"></div>
           <div className="profile-content">
-            <S.NickName>지니님</S.NickName>
-            <S.Stamp>3개</S.Stamp>
+            <NickName>지니님</NickName>
+            <Stamp>3개</Stamp>
           </div>
         </div>
         <div>
           <Link href="/mypage/settings" passHref>
-            <S.Setting>설정</S.Setting>
+            <Setting>설정</Setting>
           </Link>
         </div>
-      </S.Header>
-      <S.MyPage>
+      </Header>
+      <MyPage>
         <div className="store-list">내 이름님이 다녀간 오마카세 리스트</div>
         {dummys.map((dummy) => (
           <VisitedStore
@@ -35,9 +34,59 @@ const index = () => {
             date={dummy.date}
           />
         ))}
-      </S.MyPage>
+      </MyPage>
     </MyPageLayout>
   );
 };
 
 export default index;
+
+const Header = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1rem;
+  height: 150px;
+  background-color: #f1f1f1;
+
+  .profile {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+  .profile-image {
+    background-color: #c4c4c4;
+    border-radius: 50%;
+    width: 80px;
+    height: 80px;
+  }
+  .profile-content {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const NickName = styled.h1`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+const Stamp = styled.h2`
+  font-size: 16px;
+`;
+
+const Setting = styled.button`
+  background-color: #c4c4c4;
+  border-radius: 15px;
+  border: none;
+  padding: 5px 13px; ;
+`;
+
+const MyPage = styled.div`
+  padding: 1rem;
+
+  .store-list {
+    padding: 1rem;
+    font-weight: bold;
+  }
+`;
