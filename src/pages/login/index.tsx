a838d5fb448535e9604c1abd286939231bf75e9e
박종호 <jong951005@gmail.com>
2021-09-29 00:25:14 +0900

    닉네임 정하는 페이지 개발

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,32 +1,99 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 import Mascot from '@assets/mascot.svg';
 import LoginLayout from '@components/Layout/LoginLayout';
 
-const index = () => {
+type ErrorType = 'default' | 'duplicate' | 'usable';
+
+const errorStatusMsg = {
+  default: '2자 이상 입력해주세요',
+  duplicate: '중복된 닉네임입니다',
+  usable: '사용가능한 닉네임입니다',
+};
+
+const errorStatusColor = {
+  default: '#000',
+  duplicate: '#FF473B',
+  usable: '#2334CF',
+};
+
+const Index = () => {
+  const [errorStatus] = useState<ErrorType>('usable');
+
   return (
     <LoginLayout>
       <Content>
         <Mascot />
-        <div>어서오세요!</div>
+        <div className="welcome-letter">어서오세요!</div>
+        <div className="welcome-sub-letter">닉네임을 정해주세요 (최대 10자)</div>
+        <div className="nickname-input">
+          <input type="text" placeholder="닉네임 입력" maxLength={10} />
+          <ErrorNotifySpan errorStatus={errorStatus}>{errorStatusMsg[errorStatus]}</ErrorNotifySpan>
+        </div>
       </Content>
-      <NextPageButton>다음</NextPageButton>
+      <NextPageButton errorStatus={errorStatus}>다음</NextPageButton>
     </LoginLayout>
   );
 };
 
-export default index;
+export default Index;
 
 const Content = styled.section`
+  box-sizing: border-box;
   width: 100%;
   height: 310px;
   margin-top: 6rem;
   padding: 0 20px;
 
-  background-color: blue;
+  svg {
+    display: block;
+    margin: 0 auto;
+  }
+
+  .welcome-letter {
+    font-weight: bold;
+    font-size: 30px;
+    line-height: 36px;
+    margin: 29px 0 6px 0;
+  }
+
+  .welcome-sub-letter {
+    font-weight: 600;
+    font-size: 14px;
+    margin-bottom: 40px;
+  }
+
+  .nickname-input {
+    position: relative;
+    width: 100%;
+  }
+
+  input {
+    all: unset;
+    width: 100%;
+    height: 29px;
+    border-bottom: 1px solid #000;
+
+    color: #000;
+
+    ::placeholder {
+      color: #b8b8b8;
+    }
+  }
 `;
 
-const NextPageButton = styled.button`
+const ErrorNotifySpan = styled.span<{ errorStatus: ErrorType }>`
+  position: absolute;
+  bottom: 4px;
+  right: 0;
+  line-height: 18px;
+  font-size: 12px;
+
+  color: ${({ errorStatus }) => errorStatusColor[errorStatus]};
+`;
+
+const NextPageButton = styled.button<{ errorStatus: ErrorType }>`
   border: none;
   position: absolute;
   left: 20px;
@@ -37,7 +104,7 @@ const NextPageButton = styled.button`
   margin: 0 auto;
   border-radius: 8px;
 
-  background-color: #d7d8dd;
+  background-color: ${({ errorStatus }) => (errorStatus === 'usable' ? '#2334CF' : '#d7d8dd')};
   color: #fff;
   font-size: 18px;
   font-weight: 700;
