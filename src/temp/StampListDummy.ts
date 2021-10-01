df8f83080eeea03ccb11ca33a674f204496b125d
HyojinKim <gywls00100@gmail.com>
2021-10-02 00:20:00 +0900

    도장 개수 dummy data 생성

---
+++ src/temp/StampListDummy.ts
@@ -0,0 +1,32 @@
+export const dummys = [
+  {
+    id: 1,
+    nickName: '오마카세에대출박는',
+    count: 100,
+  },
+  {
+    id: 2,
+    nickName: '이랏샤이마세',
+    count: 90,
+  },
+  {
+    id: 3,
+    nickName: '오모',
+    count: 80,
+  },
+  {
+    id: 4,
+    nickName: '오마카세',
+    count: 70,
+  },
+  {
+    id: 5,
+    nickName: '기묘진',
+    count: 60,
+  },
+  {
+    id: 6,
+    nickName: '꿀꿀',
+    count: 50,
+  },
+];
