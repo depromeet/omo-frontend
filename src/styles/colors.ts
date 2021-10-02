88c728bd2ed40874acf839890f4af73c9223de8d
KG <acrkdduf@naver.com>
2021-10-02 21:36:00 +0900

    color 관련 theme 파일 정의

---
+++ src/styles/colors.ts
@@ -0,0 +1,43 @@
+import { css } from 'styled-components';
+
+export const colors = {
+  mainBlue: css`
+    color: '#2938BE';
+  `,
+  pointYellow: css`
+    color: '#FFC737';
+  `,
+  pointRed: css`
+    color: '#FF473B';
+  `,
+  black900: css`
+    color: '#1B1B1D';
+  `,
+  black800: css`
+    color: '#424247';
+  `,
+  black700: css`
+    color: '#54545A';
+  `,
+  black600: css`
+    color: '#707077';
+  `,
+  black500: css`
+    color: '#8C8C95';
+  `,
+  black400: css`
+    color: '#B0B0BB';
+  `,
+  black300: css`
+    color: '#D6D6DF';
+  `,
+  black200: css`
+    color: '#F1F1F4';
+  `,
+  black100: css`
+    color: '#F7F7F7';
+  `,
+  white: css`
+    color: '#FFFFFF';
+  `,
+};
