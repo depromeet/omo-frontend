6a9dc54bcbb3100b185bd3dcc6627e462ce9c1fa
KG <acrkdduf@naver.com>
2021-10-02 21:37:11 +0900

    기본폰트 pretendard 적용 및 마진 여백 20px로 적용과 theme 파일 적용

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -9,10 +9,16 @@ const GlobalStyle = createGlobalStyle`
     src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
     font-weight: 400;
     font-style: normal;
-}
+  }
+
+  * {
+    font-family: 'Pretendard-Regular';
+    font-style: normal;
+    letter-spacing: -0.02em;
+  }
 
   .container {
-    padding: 0 21px;
+    padding: 0 20px;
   }
 `;
 
--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -3,13 +3,34 @@ import { css } from 'styled-components';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
-
     colors: {
-      primary: string;
+      mainBlue: css;
+      pointYellow: css;
+      pointRed: css;
+      black900: css;
+      black800: css;
+      black700: css;
+      black600: css;
+      black500: css;
+      black400: css;
+      black300: css;
+      black200: css;
+      black100: css;
+      white: css;
     };
     fonts: {
-      regular: css;
-      bold: css;
+      header1: css;
+      header2: css;
+      header2_5: css;
+      header3: css;
+      header3_5: css;
+      subTitle1: css;
+      subTitle2: css;
+      subTitle3: css;
+      contents1: css;
+      contents2: css;
+      contents3: css;
+      contentsBold: css;
     };
   }
 }
