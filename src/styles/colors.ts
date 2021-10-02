4611f64d6e664b39068ef39838b5f979c3a2cfde
KG <acrkdduf@naver.com>
2021-10-02 21:51:05 +0900

    오타 수정

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -2,42 +2,42 @@ import { css } from 'styled-components';
 
 export const colors = {
   mainBlue: css`
-    color: '#2938BE';
+    color: #2938be;
   `,
   pointYellow: css`
-    color: '#FFC737';
+    color: #ffc737;
   `,
   pointRed: css`
-    color: '#FF473B';
+    color: #ff473b;
   `,
   black900: css`
-    color: '#1B1B1D';
+    color: #1b1b1d;
   `,
   black800: css`
-    color: '#424247';
+    color: #424247;
   `,
   black700: css`
-    color: '#54545A';
+    color: #54545a;
   `,
   black600: css`
-    color: '#707077';
+    color: #707077;
   `,
   black500: css`
-    color: '#8C8C95';
+    color: #8c8c95;
   `,
   black400: css`
-    color: '#B0B0BB';
+    color: #b0b0bb;
   `,
   black300: css`
-    color: '#D6D6DF';
+    color: #d6d6df;
   `,
   black200: css`
-    color: '#F1F1F4';
+    color: #f1f1f4;
   `,
   black100: css`
-    color: '#F7F7F7';
+    color: #f7f7f7;
   `,
   white: css`
-    color: '#FFFFFF';
+    color: #ffffff;
   `,
 };
