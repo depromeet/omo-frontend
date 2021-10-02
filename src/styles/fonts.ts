1034c40e862ef69740e7d444e572b0d690442536
KG <acrkdduf@naver.com>
2021-10-02 21:36:21 +0900

    fonts 관련 theme 파일 정의

---
+++ src/styles/fonts.ts
@@ -0,0 +1,64 @@
+import { css } from 'styled-components';
+
+export const fonts = {
+  header1: css`
+    font-weight: bold;
+    font-size: 30px;
+    line-height: 18px;
+  `,
+  header2: css`
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 18px;
+  `,
+  header2_5: css`
+    font-weight: normal;
+    font-size: 24px;
+    line-height: 18px;
+  `,
+  header3: css`
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 18px;
+  `,
+  header3_5: css`
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 18px;
+  `,
+  subTitle1: css`
+    font-weight: bold;
+    font-size: 18px;
+    line-height: 18px;
+  `,
+  subTitle2: css`
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 18px;
+  `,
+  subTitle3: css`
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 18px;
+  `,
+  contents1: css`
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 18px;
+  `,
+  contents2: css`
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 18px;
+  `,
+  contents3: css`
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 18px;
+  `,
+  contentsBold: css`
+    font-weight: bold;
+    font-size: 12px;
+    line-height: 18px;
+  `,
+};
