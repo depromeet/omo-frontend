4291fd96fb27d8c7166c55840cd2246f2c107db4
박종호 <jong951005@gmail.com>
2021-10-03 16:52:20 +0900

    Button 컴포넌트 삼항 연산자 삭제

--- src/components/Shared/Button/Button.tsx
+++ src/components/Shared/Button/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 
 const Button = ({ text, bgColor, width, clickListener }: ButtonProps) => {
   return (
-    <S.Button onClick={clickListener} bgColor={bgColor ? bgColor : ''} width={width ? width : ''}>
+    <S.Button onClick={clickListener} bgColor={bgColor} width={width}>
       {text}
     </S.Button>
   );
