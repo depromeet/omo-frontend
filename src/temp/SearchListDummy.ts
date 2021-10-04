7027a1d9954473de894b207e81cb743211fe635b
KG <acrkdduf@naver.com>
2021-10-04 20:10:37 +0900

    효진스시 매장 더미데이터에 추가!

--- src/temp/SearchListDummy.ts
+++ src/temp/SearchListDummy.ts
@@ -78,4 +78,37 @@ export const dummys: DetailPageProps[] = [
     tel: '02) 15-690',
     price: '디너 1,300,000 ~',
   },
+  {
+    id: 8,
+    image: `https://c2.poing.co.kr/place/image/thumbnail/MjAyMTA5/16310003326137170cb6960.jpg`,
+    types: ['강동구', '미들'],
+    name: `효진스시`,
+    desc: `오마카세 경력 25년의 셰프와 산지직송
+제철 식자재의 정성이 담긴 요리`,
+    location: '서울특별시 은평구 저기 어딘가',
+    tel: '02) 15-690',
+    price: '디너 1,300,000 ~',
+  },
+  {
+    id: 9,
+    image: `https://c2.poing.co.kr/place/image/thumbnail/MjAyMTA5/16310037566137246cbeafc.jpg`,
+    types: ['강북구', '미들'],
+    name: `스시왕효진`,
+    desc: `오마카세 경력 25년의 셰프와 산지직송
+제철 식자재의 정성이 담긴 요리`,
+    location: '서울특별시 은평구 저기 어딘가',
+    tel: '02) 15-690',
+    price: '디너 1,300,000 ~',
+  },
+  {
+    id: 10,
+    image: `https://c2.poing.co.kr/place/image/thumbnail/MjAyMTA5/163100064361371843a663c.jpg`,
+    types: ['강남구', '미들'],
+    name: `스시효진`,
+    desc: `오마카세 경력 25년의 셰프와 산지직송
+제철 식자재의 정성이 담긴 요리`,
+    location: '서울특별시 은평구 저기 어딘가',
+    tel: '02) 15-690',
+    price: '디너 1,300,000 ~',
+  },
 ];
