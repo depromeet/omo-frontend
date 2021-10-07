20267fc621076cb7fcaea0ed24316ccb2aea0ffd
KG <acrkdduf@naver.com>
2021-10-07 20:02:13 +0900

    Button 컴포넌트 disalbed Props 추가

--- src/components/Shared/Button/Button.tsx
+++ src/components/Shared/Button/Button.tsx
@@ -5,12 +5,19 @@ interface ButtonProps {
   bgColor?: string;
   color?: string;
   width?: string;
+  disabled?: boolean;
   clickListener: () => void;
 }
 
-const Button = ({ text, bgColor, color, width, clickListener }: ButtonProps) => {
+const Button = ({ text, bgColor, color, width, disabled, clickListener }: ButtonProps) => {
   return (
-    <S.Button onClick={clickListener} bgColor={bgColor} width={width} color={color}>
+    <S.Button
+      onClick={clickListener}
+      bgColor={bgColor}
+      width={width}
+      color={color}
+      disabled={disabled}
+    >
       {text}
     </S.Button>
   );
