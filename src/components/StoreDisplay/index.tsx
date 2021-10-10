b065ffb62891a990dd348297ab21b94d6aafc1c3
KG <acrkdduf@naver.com>
2021-10-10 21:34:57 +0900

    기존 StoreDisplay 컴포넌트 디자인 변경에 따라 와이드/리스트 형태로 세분화하여 내보내기 설정

--- src/components/StoreDisplay/index.tsx
+++ src/components/StoreDisplay/index.tsx
@@ -1,1 +1,11 @@
+export interface StoreDisplayProps {
+  id: number;
+  types: string[];
+  image: string;
+  name: string;
+  location: string;
+}
+
 export { default } from './StoreDisplay';
+export { default as StoreDisplayWide } from './StoreDisplayWide';
+export { default as StoreDisplayList } from './StoreDisplayList';
