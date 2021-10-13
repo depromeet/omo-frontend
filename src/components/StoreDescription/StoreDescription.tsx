a6842c8c938b13f63cc561190b6a64609f1e548e
KG <acrkdduf@naver.com>
2021-10-13 17:10:54 +0900

    도장깨기 버튼 클릭 시 인증 페이지로 이동하도록 라우터 설정

--- src/components/StoreDescription/StoreDescription.tsx
+++ src/components/StoreDescription/StoreDescription.tsx
@@ -1,3 +1,5 @@
+import router from 'next/router';
+
 import CopyIcon from '@assets/copy.svg';
 import FavoriteIcon from '@assets/favorite.svg';
 import TelIcon from '@assets/telephone.svg';
@@ -61,7 +63,7 @@ const StoreDescription = ({ store }: StoreDescriptionProps) => {
           <span className="count">125</span>
         </S.LikeButton>
         <Button
-          clickListener={() => alert('문의중')}
+          clickListener={() => router.push('/certification')}
           color="#fff"
           bgColor="#293AD2"
           text="이 가게 도장깨기"
