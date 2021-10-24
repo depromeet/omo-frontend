ea563596ba97c2c3fd6b0aa09b4db238168b2537
박종호 <jong951005@gmail.com>
2021-10-24 22:54:09 +0900

    ProfileImage 구현

--- src/components/ProfileImage/ProfileImage.tsx
+++ src/components/ProfileImage/ProfileImage.tsx
@@ -1,44 +1,58 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useRef } from 'react';
 
+import ProfileEraseImage from '@assets/profile-erase-image.svg';
+import { UNDEF } from '@constants/shared';
+
 import * as S from './styles';
 
 interface IProfileImage {
   setProfileImage: (file: File) => void;
+  removeProfileImage: () => void;
   thumbnail?: File;
 }
 
-const ProfileImage = ({ thumbnail, setProfileImage }: IProfileImage) => {
+const ProfileImage = ({ thumbnail, removeProfileImage, setProfileImage }: IProfileImage) => {
   const imgRef = useRef<HTMLImageElement>(null);
 
-  const onChangeProfileImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const addProfileImage = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
-
-    if (e.target.files) {
-      const file = e.target.files[0];
-      setProfileImage(file);
-    }
+    if (e.target.files) setProfileImage(e.target.files[0]);
   };
 
+  /**
+   * thumbnail이 있을 경우,
+   * imgRef의 src 에 해당 thumbnail을 넣어줍니다.
+   */
   useEffect(() => {
     if (!thumbnail) return;
 
     const reader = new FileReader();
+    const setImageOnSrc = () => {
+      if (imgRef.current === null || reader.result === null) return;
+      imgRef.current.src = reader.result as string;
+    };
+
     reader.readAsDataURL(thumbnail);
-    reader.addEventListener(
-      'load',
-      () => {
-        if (imgRef.current === null || reader.result === null) return;
-        imgRef.current.src = reader.result as string;
-      },
-      false,
-    );
+    reader.addEventListener('load', setImageOnSrc);
   }, [thumbnail]);
 
   return (
     <S.ProfileImageWrapper className="icon-selector">
-      <img src="/default_profile.png" alt="profile" ref={imgRef} />
-      <input type="file" accept="image/*, video/*" onChange={onChangeProfileImage} />
+      <S.Circle />
+      <S.DefaultImage />
+      {thumbnail !== UNDEF && (
+        <>
+          <S.CircleAura />
+          <S.ProfileImage ref={imgRef} />
+          <S.ProfileEraseButton onClick={removeProfileImage}>
+            <ProfileEraseImage />
+          </S.ProfileEraseButton>
+        </>
+      )}
+      {thumbnail === UNDEF && (
+        <input type="file" accept="image/*, video/*" onChange={addProfileImage} />
+      )}
     </S.ProfileImageWrapper>
   );
 };
