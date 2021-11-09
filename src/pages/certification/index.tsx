b378792392cfbe1a1349151213cb306168592e6b
KG <acrkdduf@naver.com>
2021-11-10 00:35:46 +0900

    장소 변경을 위한 액션 바텀 시트 추가 및 컨트롤을 위한 상태 관리: 스와이프 기능 미구현

--- src/pages/certification/index.tsx
+++ src/pages/certification/index.tsx
@@ -1,30 +1,41 @@
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-import { LocationChecker, ReceiptChecker } from '@components/Certification';
+import { SearchBottomActionSheet as StyledSearchBottomActionSheet } from '@/components/Certification/styles';
+import {
+  LocationChecker,
+  ReceiptChecker,
+  SearchBottomActionSheet,
+} from '@components/Certification';
 import Header from '@components/Header';
 import Button from '@components/Shared/Button';
 import { CertificationModal } from '@components/Shared/Modal';
 
 const Certification = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isActionSheetActive, setIsActionSheetActive] = useState(false);
   const {
     query: { blobUrl, image, location, name },
   } = useRouter();
+
+  const handleClickOnReselectLocation = () => {
+    setIsActionSheetActive((prev) => !prev);
+  };
 
   useEffect(() => {
     return () => URL.revokeObjectURL(blobUrl as string);
   }, [blobUrl]);
 
   return (
-    <CertificationPage>
+    <CertificationPage isActionSheetActive={isActionSheetActive}>
       <Header title="인증확인" />
       <CertificationMain className="container">
         <LocationChecker
           image={image as string}
           location={location as string}
           name={name as string}
+          handleClickOnReselectLocation={handleClickOnReselectLocation}
         />
 
         <ReceiptChecker blobUrl={blobUrl as string} />
@@ -36,17 +47,39 @@ const Certification = () => {
           text="도장깨기 접수하기"
         />
       </CertificationMain>
+
       {isModalOpen && <CertificationModal name={name as string} />}
+      <SearchBottomActionSheet handleClickOnReselectLocation={handleClickOnReselectLocation} />
     </CertificationPage>
   );
 };
 
 export default Certification;
 
-const CertificationPage = styled.section`
+const CertificationPage = styled.section<{ isActionSheetActive: boolean }>`
+  position: relative;
   display: flex;
   flex-direction: column;
   height: 100vh;
+
+  ${({ isActionSheetActive }) =>
+    isActionSheetActive &&
+    css`
+      ${StyledSearchBottomActionSheet} {
+        transform: translateY(0);
+      }
+
+      &::after {
+        content: '';
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        background-color: rgba(0, 0, 0, 0.4);
+        z-index: 10;
+      }
+    `}
 `;
 
 const CertificationMain = styled.article`
