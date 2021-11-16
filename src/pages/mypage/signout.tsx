b200f54655d5df52c46fe10d3d9514c0c6ce38bf
박종호 <jong951005@gmail.com>
2021-11-16 17:55:39 +0900

    회원탈퇴 API 연동

--- src/pages/mypage/signout.tsx
+++ src/pages/mypage/signout.tsx
@@ -1,13 +1,32 @@
-// 회원탈퇴 페이지
+import { useRouter } from 'next/router';
 import styled from 'styled-components';
 
+import { requestError } from '@@types/shared';
 import ModalLayout from '@components/Layout/ModalLayout';
+import { NETWORK_ERROR, UNKNOWN_ERROR } from '@constants/error';
+import { requestDeleteUser } from '@request';
+import { showAlertModal, showConfirmModal } from '@utils/modal';
 
 const Signout = () => {
-  const onClick = () => alert('탈퇴가 완료되었습니다.');
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    if (!showConfirmModal('정말 탈퇴하시겠습니까?')) return;
+
+    try {
+      await requestDeleteUser();
+      showAlertModal('탈퇴가 완료되었습니다.');
+      router.push('/');
+    } catch (error) {
+      const { response } = error as requestError;
+      if (!response) return showAlertModal(NETWORK_ERROR);
+
+      return showAlertModal(UNKNOWN_ERROR);
+    }
+  };
 
   return (
-    <ModalLayout title="회원탈퇴" buttonContent="탈퇴하기" clickHandler={onClick}>
+    <ModalLayout title="회원탈퇴" buttonContent="탈퇴하기" clickHandler={handleSignOut}>
       <SignOutPage>
         탈퇴시 도장 깬 내역을 포함한 <br />
         모든 정보가 초기화됩니다.
