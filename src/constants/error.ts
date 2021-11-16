c155e3f7c7c420f085ba0bacee889ad798f8223f
박종호 <jong951005@gmail.com>
2021-11-16 14:43:16 +0900

    error handling 코드 추가

---
+++ src/constants/error.ts
@@ -0,0 +1,6 @@
+export const NICKNAME_ERROR: { [key: number]: string } = {
+  409: '존재하는 닉네임은 사용할 수 없습니다.',
+};
+
+export const NETWORK_ERROR = '서버와 통신이 되지 않습니다. 단말기의 네트워크를 확인해보세요.';
+export const UNKNOWN_ERROR = '알 수 없는 에러가 발생했습니다.';
