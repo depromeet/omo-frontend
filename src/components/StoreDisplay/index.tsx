49922d9e42aa9d9a1da9f0fb702cf59ee41601c0
KG <acrkdduf@naver.com>
2021-11-25 18:35:35 +0900

    refactor: 기존 더미데이터에서 사용하던 불필요 타입 제거

--- src/components/StoreDisplay/index.tsx
+++ src/components/StoreDisplay/index.tsx
@@ -1,12 +1,3 @@
-export interface StoreDisplayProps {
-  id: number;
-  level: string;
-  image_url: string;
-  name: string;
-  address: string;
-  county: string;
-}
-
 export { default } from './StoreDisplay';
 export { default as StoreDisplayWide } from './StoreDisplayWide';
 export { default as StoreDisplayList } from './StoreDisplayList';
