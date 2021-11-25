f72df3b54f145a3171764afa47c5d35d92d8f5ee
KG <acrkdduf@naver.com>
2021-11-25 18:32:49 +0900

    refactor: 기존 제보하기 모달 페이지 제거 후 텍스트 문구 받아 데이터 없음 띄워주도록 변경

--- src/components/SearchNoData/SearchNoData.tsx
+++ src/components/SearchNoData/SearchNoData.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { useState } from 'react';
 
 import Button from '@components/Shared/Button';
@@ -6,39 +7,51 @@ import { HashTag } from '@components/StoreDescription/styles';
 
 import * as S from './styles';
 
+// 제보하기 관련 모달 버튼 포함된 no-data 페이지 잠시 보류
 interface SearchNoDataProps {
   keyword: string;
 }
 
+// const SearchNoData = ({ keyword }: SearchNoDataProps) => {
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+
+//   const toggleModal = () => {
+//     setIsModalOpen((prev) => !prev);
+//   };
+
+//   return (
+//     <S.NoData>
+//       <div className="guide-message-wrapper">
+//         <p className="guide-message">&quot;{keyword}&quot;에 대한 검색결과가 없어요!</p>
+//         <p className="guide-message">혹시, 이런 검색어는 어떠세요?</p>
+//       </div>
+
+//       <div className="hashtag-wrapper">
+//         <HashTag>#강남구</HashTag>
+//         <HashTag>#강동구</HashTag>
+//         <HashTag>#미들</HashTag>
+//       </div>
+
+//       <S.Divider />
+
+//       <div className="guide-message-wrapper mt-30">
+//         <p className="guide-message">원하는 가게를 찾지 못하셨나요?</p>
+//         <p className="guide-message">오모에 제보해주세요!</p>
+//       </div>
+
+//       <Button clickListener={toggleModal} text="가게 제보하기" bgColor="#FFC737" color="#fff" />
+//       {isModalOpen && <SuggestModal toggleModal={toggleModal} />}
+//     </S.NoData>
+//   );
+// };
+
 const SearchNoData = ({ keyword }: SearchNoDataProps) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const toggleModal = () => {
-    setIsModalOpen((prev) => !prev);
-  };
-
   return (
     <S.NoData>
-      <div className="guide-message-wrapper">
-        <p className="guide-message">&quot;{keyword}&quot;에 대한 검색결과가 없어요!</p>
-        <p className="guide-message">혹시, 이런 검색어는 어떠세요?</p>
-      </div>
-
-      <div className="hashtag-wrapper">
-        <HashTag>#강남구</HashTag>
-        <HashTag>#강동구</HashTag>
-        <HashTag>#미들</HashTag>
-      </div>
-
-      <S.Divider />
-
-      <div className="guide-message-wrapper mt-30">
-        <p className="guide-message">원하는 가게를 찾지 못하셨나요?</p>
-        <p className="guide-message">오모에 제보해주세요!</p>
-      </div>
-
-      <Button clickListener={toggleModal} text="오마카세 제보하기" bgColor="#FFC737" color="#fff" />
-      {isModalOpen && <SuggestModal toggleModal={toggleModal} />}
+      <S.ImageWrapper>
+        <Image src="/images/snowman.png" alt="마스코트" layout="fill" />
+      </S.ImageWrapper>
+      <S.NoDataMessage>{keyword}</S.NoDataMessage>
     </S.NoData>
   );
 };
