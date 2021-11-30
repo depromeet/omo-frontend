3e9ad2494cf666d2de6aac5edaad0519707b2046
KG <acrkdduf@naver.com>
2021-12-01 00:52:19 +0900

    style: flex의 가운데정렬 제거하고 margin-top 으로 변경

--- src/components/SearchResults/styles.ts
+++ src/components/SearchResults/styles.ts
@@ -16,7 +16,5 @@ export const LoadingSection = styled.div`
 export const NoDataWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
   flex: 1;
 `;
