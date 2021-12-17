af1e9469ab65439effa2ceb81b869a9c8732fd94
Jongho Park <jong951005@gmail.com>
2021-12-17 23:56:56 +0900

    이미지 재 수정

--- src/components/StoreDisplay/StoreDisplayList.tsx
+++ src/components/StoreDisplay/StoreDisplayList.tsx
@@ -4,8 +4,11 @@ import Link from 'next/link';
 import { Omakases } from '@recoil/omakaseState';
 
 import * as S from './styles';
+import { DEFAULT_IMAGE_URL } from '@constants/omakase';
 
 const StoreDisplay = ({ id, image_url, level, county, name, address }: Omakases) => {
+  image_url = image_url ? `${process.env.API_ENDPOINT}${image_url}` : DEFAULT_IMAGE_URL;
+
   return (
     <S.StoreDisplay>
       <Link href={`/search/${id}`} passHref>
@@ -22,7 +25,7 @@ const StoreDisplay = ({ id, image_url, level, county, name, address }: Omakases)
 
             <S.StoreImageWrapper className="mode-list">
               <Image
-                src={image_url}
+                src={image_url ? `${process.env.API_ENDPOINT}${image_url}` : DEFAULT_IMAGE_URL}
                 alt="매장 이미지 미리보기"
                 width={200}
                 height={160}
--- src/components/StoreDisplay/StoreDisplayWide.tsx
+++ src/components/StoreDisplay/StoreDisplayWide.tsx
@@ -7,8 +7,6 @@ import * as S from './styles';
 import { DEFAULT_IMAGE_URL } from '@constants/omakase';
 
 const StoreDisplay = ({ id, image_url, level, county, name, address }: Omakases) => {
-  image_url = image_url ? `${process.env.API_ENDPOINT}${image_url}` : DEFAULT_IMAGE_URL;
-
   return (
     <S.StoreDisplay>
       <Link href={`/search/${id}`} passHref>
@@ -19,7 +17,11 @@ const StoreDisplay = ({ id, image_url, level, county, name, address }: Omakases)
               <span>#{county}</span>
             </S.SubTitles>
 
-            <Image src={image_url} alt="매장 이미지 미리보기" layout="fill" />
+            <Image
+              src={image_url ? `${process.env.API_ENDPOINT}${image_url}` : DEFAULT_IMAGE_URL}
+              alt="매장 이미지 미리보기"
+              layout="fill"
+            />
 
             <S.StoreDescriptionWrapper>
               <h1 className="store-title">{name}</h1>
